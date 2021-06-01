55dc7b7c830f37cfebf94d0cbfb7e51440d173cd
Tr3yb0 <tbotard@flur.ee>
2021-06-01 15:54:57 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { FlureeConn, FlureeProvider, useFlureeQuery } from '@fluree/react-wrapper';
+
+
+// import '@fluree/react-wrapper/dist/index.css'
+const myconn = new FlureeConn({
+  servers: "http://localhost:8090",
+  ledger: "example/ledger",
+  workerUrl: "/flureeworker.js"
+});
+
+const query = {
+  select: [
+    "*"
+  ],
+  from: "_collection"
+}
+
+function ShowCollections(){
+  const data = useFlureeQuery(query);
+  const { result, loading } = data;
+  if (loading) {
+    return (<div>Loading....</div>);
+  } else {
+  return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Doc</th>
+              <th>Version</th>
+            </tr>
+          </thead>
+          <tbody>
+            {result.map(collection => (
+              <tr key={collection._id}>
+                <td>{collection._id}</td>
+                <td>{collection.name}</td>
+                <td>{collection.doc}</td>
+                <td>{collection.version}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+  )
+  };
+}
+
+const App = () => {
+  // return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <FlureeProvider conn={myconn}>
+      <ShowCollections />
+    </FlureeProvider>
+  )
+}
+
+export default App
